5a2507c452bc12c75ab341744043c22da8b9efab
José Luis Segura Lucas <josel.segura@gmx.es>
2012-10-25 08:52:11 +0200

    First approach to the SMS handler

--- Power Notifier/src/org/barrabash/powernotifier/ChargingReceiver.java
+++ Power Notifier/src/org/barrabash/powernotifier/ChargingReceiver.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,23 +19,35 @@ public class ChargingReceiver extends BroadcastReceiver {
 		
 		Log.wtf(PowerNotifierActivity.TAG, action);
 		
-		;SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
 		String[] array;
+		boolean sent = false;
 		
 		if(sharedPref.getBoolean("send_email", false)) {
 			Set<String> email_addresses = sharedPref.getStringSet("email_list", new HashSet<String>());
-			array = email_addresses.toArray(new String[0]);
-			new MailAsyncTask(ctx).execute(array);
-			Toast.makeText(ctx,  "E-mail",  Toast.LENGTH_SHORT).show();
+			if(email_addresses.size() > 0) {
+				array = email_addresses.toArray(new String[0]);
+			
+				new MailAsyncTask(ctx).execute(array);
+				sent = true;
+			}
 		}
 		
 		if(sharedPref.getBoolean("send_sms", false)) {
 			Set<String> sms_phones = sharedPref.getStringSet("sms_list", new HashSet<String>());
-			array = sms_phones.toArray(new String[0]);
-			// send SMS
-			Toast.makeText(ctx,  "SMS",  Toast.LENGTH_SHORT).show();
+			if(sms_phones.size() > 0) {
+				SmsManager smsMg = SmsManager.getDefault();
+				
+				for(String phone : sms_phones)
+					smsMg.sendTextMessage(phone, null, "TEST", null, null);
+					
+				sent = true;
+			}
+			
 		}
 		
+		if (sent)
+			Toast.makeText(ctx, ctx.getString(R.string.sent),  Toast.LENGTH_SHORT).show();
 		
 	}
 }
